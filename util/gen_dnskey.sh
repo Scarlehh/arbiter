087caaa98b04ddb5ba9356c0095131547b57066e
Scarlett <gourleys@tcd.ie>
2018-03-22 15:52:34 +0000

    Add utility scripts for generating dnskeys and certificates

---
+++ util/gen_dnskey.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Need to supply zone name"
+	exit 1;
+fi
+
+if [ -z "$2" ] || [ "$2" == "ZSK" ]; then
+	dnssec-keygen -a ECDSAP256SHA256 -b 256 -n ZONE $1
+else
+	if [ "$2" == "KSK"]; then
+		dnssec-keygen -a ECDSAP256SHA256 -b 256 -n ZONE $1
+	else
+		echo "Second argument 'KSK' if you wish to generate a key signing key"
+		exit 1;
+	fi
+fi
---
+++ util/gen_selfsign.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1"]; then
+	echo "Need to supply cert name"
+else
+	if [ -z "$2" ]; then
+		echo "Generating ECDSAP256SHA256 key"
+		openssl req -new -x509 -nodes -days 365 \
+				-newkey ec:<(openssl ecparam -name prime256v1) \
+				-keyout $1.key \
+				-out $1.crt
+	else
+		openssl req -new -x509 -days 365 \
+				-key $2 \
+				-out $1.crt
+	fi
+fi
